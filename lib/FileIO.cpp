6b15d336f017f1a640ea16c63627d518cfdb9948
pijar <pijarpahlawanq@students.ac.id>
2022-06-20 21:39:54 +0700

    add docs

--- lib/FileIO.cpp
+++ lib/FileIO.cpp
@@ -1,11 +1,22 @@
 #include "FileIO.h"
 
+/**
+ * @brief Construct a new FileIO:: FileIO object
+ *
+ * @param numof_datas jumlah data
+ */
 FileIO::FileIO(int numof_datas)
 {
     this->size = numof_datas;
     datas = new std::string[this->size];
 }
 
+/**
+ * @brief membaca data dari file dan mengembalikan data dalam bentuk string
+ *
+ * @param name nama file
+ * @return std::string*
+ */
 std::string *FileIO::read(const char *name)
 {
     file.open(name, std::ios::in);
@@ -19,8 +30,12 @@ std::string *FileIO::read(const char *name)
     return datas;
 }
 
-// TODO: definisi
-
+/**
+ * @brief menulis suatu data string ke dalam file
+ *
+ * @param name nama file
+ * @param datas data yang akan ditulis
+ */
 void FileIO::write(const char *name, std::string *datas)
 {
     file.open(name, std::ios::out);
