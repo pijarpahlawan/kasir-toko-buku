3fbf1f3505f1b117a531dcc394581627d0c2bac2
pijar <pijarpahlawanq@students.ac.id>
2022-06-25 00:45:36 +0700

    add new member

--- lib/Buku.h
+++ lib/Buku.h
@@ -1,13 +1,9 @@
 #pragma once
 #include <iostream>
-using namespace std;
-
-// Jumlah buku dalam database
-int numof_book = 0;
 
 // Informasi detail buku
 struct Buku
 {
-    string judul, isbn, penulis, penerbit, th_terbit;
-    int harga;
+    std::string judul, isbn, penulis, penerbit, th_terbit;
+    int harga, jumlah;
 };
