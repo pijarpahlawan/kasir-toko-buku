376e2ed6d9b99eca419bb54d4c4e5fb019cf169d
pijar <pijarpahlawanq@students.ac.id>
2022-06-27 12:20:51 +0700

    update main

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,34 @@
-#include <iostream>
-using namespace std;
+#include "declare.h"
 
 int main()
 {
+    int menu = 0;
+
+    while (true)
+    {
+        display();
+
+        cout << "\n[1] Input data\n[2] Beli buku\n[3] Keluar" << endl;
+        cout << "Silahkan pilih: ";
+        cin >> menu;
+
+        if (menu == 1)
+        {
+            input();
+        }
+        else if (menu == 2)
+        {
+            // beli
+        }
+        else if (menu == 3)
+        {
+            break;
+        }
+        else
+        {
+            cout << "Pilih yg benar masnya" << endl;
+        }
+    }
+
     return 0;
 }
