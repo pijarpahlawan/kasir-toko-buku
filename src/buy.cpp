b09b0a059b2109ad2a8fc7ad1fe18bc43a571fe6
Pijar Pahlawan <pijarpahlawanq@students.amikom.ac.id>
2022-06-27 22:10:19 +0700

    update buying fiture

--- src/buy.cpp
+++ src/buy.cpp
@@ -21,22 +21,48 @@ void beli()
         {
             cout << "Buku Tidak Ditemukan\n";
             // TODO: tampilkan notif kosong
+            cout << "Maaf data yang Anda Cari Tidak ada" << endl;
+
             // TODO: meminta lanjut ke awal/cari lagi
-            //* if awal then return
-            return;
+            cout << "\tPilihan : " << endl;
+            cout << "\t1. Kembali ke Menu Utama " << endl;
+            cout << "\t2. Kembali ke Pencarian" << endl;
+
+            int pilihan;
+            cout << "\tPilihan menu berdasarkan angka: " << endl;
+            cin >> pilihan;
+            if (pilihan == 1)
+                return;
+            else if (pilihan != 2)
+            // notif salah
         }
         else
         {
             cout << "Buku Ditemukan!\n";
             // TODO: print buku
+            cout << "Judul buku : " << book->judul << endl;
+            cout << "ISBN       : " << book->isbn << endl;
+            cout << "Penulis    : " << book->penulis << endl;
+            cout << "Penerbit   : " << book->penerbit << endl;
+            cout << "Th. Terbit : " << book->th_terbit << endl;
+            cout << "Harga      : " << book->harga << endl;
+
             // TODO: pertanyaan jadi beli
+            cout << "Apakah anda jadi membeli buku ini?  (y/t)/n";
+            cin >> jawab;
+
             //** if jadi then
-            // TODO: pembayaran
-            // TODO: beli lagi?
-            //* if !beli_lagi then break
-            //** else (if awal then return)
-        }
-    }
-    data.write();
-    // TODO: cetak nota
+            if (jawab == 'y')
+            {
+                // TODO: pembayaran
+                cout << "Total pembayaran anda adalah: /n" << harga;
+            }
+            else if (jawab != t)
+                // notif salah
+                else break;
+            /* code */
+}
+}
+ data.write();
+// TODO: cetak nota
 }
