c8fb993c065007eabc8c4f8b103c5be7734784f6
pijar <pijarpahlawanq@students.ac.id>
2022-07-04 00:23:31 +0700

    fix bug

--- src/helpers.cpp
+++ src/helpers.cpp
@@ -2,15 +2,15 @@
 
 string fprice(int price)
 {
-
+    string price_s = to_string(price);
     int count = 0;
-    for (int i = to_string(price).size(); i > 0; i -= 3)
+    for (int i = price_s.size(); i > 0; i -= 3)
     {
         // tiap 3 karakter dari belakang maka string akan di tambahkan titik
         if (count > 0)
-            to_string(price).insert(i, ".");
+            price_s.insert(i, ".");
         count++;
     }
     // mengembalikan harga yang sudah diformat dengan currency IDR
-    return "Rp" + to_string(price);
+    return "Rp" + price_s;
 }
